4a5f71e26065e2ea026c9f847817ae618e0863f4
slaibne <ivy.hou330@icloud.com>
2024-02-04 19:24:20 -0500

    adding JUnit Tests

--- src/SalaryWorkerTest.java
+++ src/SalaryWorkerTest.java
@@ -1,2 +1,24 @@
-public class SalaryWorkerTest {
+import org.junit.jupiter.api.Test;
+
+import java.text.NumberFormat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalaryWorkerTest {
+    SalaryWorker testSalaryWorker = new SalaryWorker("000001", "Jim", "Jambo", "Mr.", 1980, 0, 52000);
+    @Test
+    void calculateWeeklyPay()
+    {
+        assert testSalaryWorker.calculateWeeklyPay(40) == (1000);
+        assert testSalaryWorker.calculateWeeklyPay(90) == (1000);
+    }
+
+    @Test
+    void displayWeeklyPay()
+    {
+        assert testSalaryWorker.displayWeeklyPay(90).equals("The weekly pay is merely the annual salary divided by 52;" +
+                " the result being $1,000.00");
+        assert testSalaryWorker.displayWeeklyPay(40).equals("The weekly pay is merely the annual salary divided by 52;" +
+                " the result being $1,000.00");
+    }
 }
--- src/WorkerTest.java
+++ src/WorkerTest.java
@@ -1,2 +1,22 @@
-public class WorkerTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkerTest {
+    Worker testWorker = new Worker("000001", "Jane", "Jambo", "Mrs.", 1982, 20);
+    @Test
+    void calculateWeeklyPay()
+    {
+        assert testWorker.calculateWeeklyPay(40) == 800;
+        assert testWorker.calculateWeeklyPay(60) == 1400;
+    }
+
+    @Test
+    void displayWeeklyPay()
+    {
+        assert testWorker.displayWeeklyPay(40).equals("Worked for 40.0 hours and gained this much money: $800.00");
+        assert testWorker.displayWeeklyPay(60).equals("From the normal time worked of 40 hours; gained: $800.00" +
+                " Then worked for another 20.0 hours of overtime and gained: $600.00 " +
+                "In total worked for 60.0 hours and gained: $1,400.00");
+    }
 }
